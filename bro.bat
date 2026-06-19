4806e8dc13db9ea5a773c159f894ca7eeca2a147
Aditya Sawant <arsawant09@gmail.com>
2026-06-19 16:22:34 -0400

    Removal of Py

--- bro.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-set BRO_EXEC=%TEMP%\bro_%RANDOM%.bat
-python R:\bro-cli\bro.py --exec-file "%BRO_EXEC%" %*
-if exist "%BRO_EXEC%" (
-    call "%BRO_EXEC%"
-    del "%BRO_EXEC%"
-)
--- bro.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-python3 /mnt/r/bro-cli/bro.py "$@"
